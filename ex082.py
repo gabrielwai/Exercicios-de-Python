0a82ea9debc3b575d6661b818ccbca2105d00da0
Gabriel Wai Ramos dos Santos <gabriel.santos261@fatec.sp.gov.br>
2020-04-27 18:25:45 -0300

    Update ex082.py

--- ex082.py
+++ ex082.py
@@ -1,8 +1,28 @@
 lista = list()
-lista.append(int(input('Digite um número inteiro: ')))
-
+pares = list()
 ímpares = list()
+continuar = ''
+
+while True:
+    lista.append(int(input('Digite um número inteiro: ')))
+
+    while continuar != 'S' and continuar != 'N':
+        continuar = str(input('Deseja continuar? [S/N]: ')).strip().upper()
+        continuar = continuar[0]
+
+        if continuar != 'S' and continuar != 'N':
+            print('Valor inválido, tente novamente.')
+
+    if continuar == 'N':
+        break
+    elif continuar == 'S':
+        continuar = ''
+
 for count in range(0, len(lista)):
     if lista[count] % 2 != 0:
-        
-pares = list()
+        ímpares.append(lista[count])
+    else:
+        pares.append(lista[count])
+
+print(f'A lista primeiro preenchida foi: {lista}\nOs números ímpares digitados foram: {ímpares}')
+print(f'Já os números pares foram: {pares}')
