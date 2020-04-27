02d81d59926070e6ef675986a7df65192acb833f
Gabriel Wai Ramos dos Santos <gabriel.santos261@fatec.sp.gov.br>
2020-04-27 17:47:53 -0300

    Atualizacao de exercicio 82-1

--- ex082.py
+++ ex082.py
@@ -1,1 +1,8 @@
 lista = list()
+lista.append(int(input('Digite um número inteiro: ')))
+
+ímpares = list()
+for count in range(0, len(lista)):
+    if lista[count] % 2 != 0:
+        
+pares = list()
