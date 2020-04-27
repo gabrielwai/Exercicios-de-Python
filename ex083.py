c1ebe15d6a69e3493f94b423725cd0c352176d5a
Gabriel Wai Ramos dos Santos <gabriel.santos261@fatec.sp.gov.br>
2020-04-27 19:41:06 -0300

    Update ex083.py

--- ex083.py
+++ ex083.py
@@ -4,3 +4,5 @@ if expressão.count(')') == expressão.count('('):
     print('Expressão válida (referente aos parênteses)')
 else:
     print('Expressão inválida, reveja os parênteses')
+# ERRADO! Basta tentar o seguinte input: 6 + ) 85 (
+# Deta forma não é possível detectar a hierarquicidade dos parênteses
