ba8315613ef8ce0a74a124e1b6271694af5506c4
Gabriel Wai Ramos dos Santos <gabriel.santos261@fatec.sp.gov.br>
2020-05-22 14:13:03 -0300

    Update somador de digitos de um numero.py

--- somador de digitos de um numero.py
+++ somador de digitos de um numero.py
@@ -1,4 +1,32 @@
 import math
-n = int(input('Digite um número inteiro a se calcular seu fatorial: '))
-print(f"O Fatorial de N é = {math.factorial(n)} = N!")
-#print(f'Aqui seu número: {math.pow(n,1)}')
+n = -1
+while n < 0:
+    n = int(input('Digite um número (N) inteiro a se calcular seu fatorial: '))
+    '''print(f"O Fatorial de N é = {math.factorial(n)} = N!")'''
+    if n < 0:
+        print("Digite apenas valores positivos. Tente novamente.",end='\n')
+
+if n == 0:
+    fatorial = 1
+else:
+    fatorial = n
+    for count in range(1, n-1):
+        fatorial *= (n-count)
+
+print(f"N! = {fatorial}")
+acumulador = fatorial
+count = 0
+while fatorial / 10 > 1:
+        fatorial /= 10
+        count += 1
+count += 1
+print(f'Numero de algarismos de N! = {count}')
+
+fatorial = acumulador
+acumulador = 0
+print(f'Fatorial = {fatorial} ; Acumulador = {acumulador}')
+
+for j in range(1, count+1):
+    acumulador += math.floor((fatorial - ((math.floor(fatorial/math.pow(10, j))) * math.pow(10, j))) / math.pow(10, j-1))
+    print(f"Soma dos algarismos: {acumulador} ; {math.floor((fatorial - ((math.floor(fatorial/math.pow(10, j))) * math.pow(10, j))) / math.pow(10, j-1))}")
+print(f"Resultado da soma dos algarismos: {acumulador}")
