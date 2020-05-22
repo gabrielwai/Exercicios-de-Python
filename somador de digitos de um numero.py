4501215d538faa0d3fcf14ecb1dcf5987c6e7a99
Gabriel Wai Ramos dos Santos <gabriel.santos261@fatec.sp.gov.br>
2020-05-22 13:22:38 -0300

    Create somador de digitos de um numero.py

---
+++ somador de digitos de um numero.py
@@ -0,0 +1,4 @@
+import math
+n = int(input('Digite um número inteiro a se calcular seu fatorial: '))
+print(f"O Fatorial de N é = {math.factorial(n)} = N!")
+#print(f'Aqui seu número: {math.pow(n,1)}')
