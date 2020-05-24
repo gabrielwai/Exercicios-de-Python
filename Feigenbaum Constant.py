b9208c617f350a8c986596c6df5d2176dfaadcc8
Gabriel Wai Ramos dos Santos <gabriel.santos261@fatec.sp.gov.br>
2020-05-24 15:09:10 -0300

    Update Feigenbaum Constant.py
    Funcional

--- Feigenbaum Constant.py
+++ Feigenbaum Constant.py
@@ -1,16 +1,30 @@
 lanbda = 5
-while lanbda > 4 or lanbda < 1:
-    lanbda = float(input('Digite um número real (entre 1 a 4 inclusive) que corresponderá ao lambda: '))
-    if lanbda > 4 or lanbda < 1:
+listaPrincipal = list()
+listaPeriodos = list()
+
+while lanbda > 4 or lanbda < 0:
+    lanbda = float(input('Digite um número real (entre 0 a 4 inclusive) que corresponderá ao lambda: '))
+    if lanbda > 4 or lanbda < 0:
         print("Valor fora do intevalo (1 a 4). Tente novamente.")
 Xn = 2
 while Xn < 0 or Xn > 1:
-    Xn = float(input('Digite um número (entre 0 a 1 inclusive) que corresponderá ao Xn: '))
+    Xn = float(input('Digite um número real (entre 0 a 1 inclusive) que corresponderá ao Xn: '))
     if Xn < 0 or Xn > 1:
         print("Valor fora do intevalo (0 a 1). Tente novamente.")
 
+contador = Xn
+periodo = 0
 Xanterior = Xn
 print(f"Xn = {Xn}")
-for count in range(1, 32):
+
+for count in range(1, 1000):
     print(f"Xn+{count} = {lanbda * Xanterior * (1 - Xanterior)}")
     Xanterior = lanbda * Xanterior * (1 - Xanterior)
+
+    listaPrincipal.append(Xanterior)
+    if listaPrincipal.count(Xanterior) > 1:
+        if (Xanterior in listaPeriodos) == False:
+            listaPeriodos.append(Xanterior)
+
+print(f'Período : {len(listaPeriodos)}')
+print(f"Termos: {listaPeriodos}")
