038a1ef02f32390517f1ea5e5d666a2c45a1a3e9
Gabriel Wai Ramos dos Santos <gabriel.santos261@fatec.sp.gov.br>
2020-05-24 00:20:24 -0300

    Create Feigenbaum Constant.py

---
+++ Feigenbaum Constant.py
@@ -0,0 +1,15 @@
+lanbda = 5
+while lanbda > 4 or lanbda < 1:
+    lanbda = float(input('Digite um número real (entre 1 a 4 inclusive) que corresponderá ao lambda: '))
+    if lanbda > 4 or lanbda < 1:
+        print("Valor fora do intevalo (1 a 4). Tente novamente.")
+Xn = 2
+while Xn < 0 or Xn > 1:
+    Xn = float(input('Digite um número (entre 0 a 1 inclusive) que corresponderá ao Xn: '))
+    if Xn < 0 or Xn > 1:
+        print("Valor fora do intevalo (0 a 1). Tente novamente.")
+
+Xanterior = Xn
+for count in range(1, 10):
+    print(f"Xn+1 = {lanbda * Xanterior * (1 - Xanterior)}")
+    Xanterior = lanbda * Xanterior * (1 - Xanterior)
