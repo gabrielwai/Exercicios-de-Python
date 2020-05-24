74a6020e46301ef801b5173d73fdb159c930d3d4
Gabriel Wai Ramos dos Santos <gabriel.santos261@fatec.sp.gov.br>
2020-05-24 17:16:53 -0300

    Update Feigenbaum Constant.py
    Funcional 1.1

--- Feigenbaum Constant.py
+++ Feigenbaum Constant.py
@@ -3,9 +3,9 @@ listaPrincipal = list()
 listaPeriodos = list()
 
 while lanbda > 4 or lanbda < 0:
-    lanbda = float(input('Digite um número real (entre 1 a 4 inclusive) que corresponderá ao lambda: '))
+    lanbda = float(input('Digite um número real (entre 0 a 4 inclusive) que corresponderá ao lambda: '))
     if lanbda > 4 or lanbda < 0:
-        print("Valor fora do intevalo (1 a 4). Tente novamente.")
+        print("Valor fora do intevalo (0 a 4). Tente novamente.")
 Xn = 2
 while Xn < 0 or Xn > 1:
     Xn = float(input('Digite um número real (entre 0 a 1 inclusive) que corresponderá ao Xn: '))
