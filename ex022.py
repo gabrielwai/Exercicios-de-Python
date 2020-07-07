30f7aaa6757f769ec2e9357ace6ca8400064e8e6
Gabriel Wai Ramos dos Santos <gabriel.santos261@fatec.sp.gov.br>
2020-07-06 22:17:58 -0300

    Update ex022 (manipulação de texto).py

--- ex022.py
+++
@@ -1,9 +0,0 @@
-nome = str(input('Digite seu nome completo: '))
-
-print("Seu nome todo em maiúsculo é: {}".format(nome.upper()))
-print("\nSeu nome todo em minúsculo é: {}".format(nome.lower()))
-print("Qtdd de espaços totais: {}".format(nome.count(' ')))
-print('Total de letras (desconsiderando os espaços): {}'.format(len(nome)-nome.count(" ")))
-print('Primeiro nome: {}'.format(nome[:nome.find(' ')]))
-print('Quantidade de letras que possui o primeiro nome: {}'.format(len(nome[:nome.find(' ')])))
-#nome teste: Wirllbda Olnd  Silva NkbsKj
