b045e7b53bf13c92e5ee5ba3756a24b343edcd9f
Gabriel Wai Ramos dos Santos <62815808+gabrielwai@users.noreply.github.com>
2020-07-31 17:54:28 -0300

    Add files via upload

--- ordenando jogos de loteria pelos valores sorteados.py
+++ ordenando jogos de loteria pelos valores sorteados.py
@@ -1,4 +1,5 @@
 import random
+import operator
 
 jogos = dict()
 lista = list()
@@ -20,3 +21,24 @@ for c in range(0, 4):
 print(jogos)
 print(lista)
 print(lista1)
+print(len(lista))
+for c in range(len(lista)):
+    print(f'{lista[c]["nome"]} => {lista[c]["valor"]}')
+    lista[c]['valor'].sort()
+print()
+for c in range(len(lista)):
+    print(f'{lista[c]["nome"]} => {lista[c]["valor"]}')
+    lista[c]['valor'] = lista[c]['valor'][0]
+print(lista,end='\n...\n')
+
+#sorted(lista, key=operator.itemgetter(lista["valores"]))
+#print(sorted(lista, key=operator.itemgetter()))
+lista1.clear()
+for c in range(len(lista)):
+    lista1.append(lista[c]['valor'])
+print(lista1)
+lista1.sort()
+print(lista1)
+print(lista['nome'].index(lista1[0]))
+for c in range(len(lista)):
+    print(F'{lista.index(lista1[0])} = {lista1[0]}')
